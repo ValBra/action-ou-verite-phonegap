e12cf88181217c787c3a2a9b2f9a395e08dfd540
AlexandreCM <alexandre.mezeray@gmail.com>
2018-11-13 11:35:56 -0500

    initialisation projet

---
+++ app/src/main/java/ca/qc/cgmatane/informatique/actionouverite/MainActivity.java
@@ -0,0 +1,18 @@
+package ca.qc.cgmatane.informatique.actionouverite;
+
+import android.os.Bundle;
+
+import org.apache.cordova.*;
+import org.apache.cordova.DroidGap;
+
+public class MainActivity extends DroidGap {
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        super.init();
+        super.loadUrl(Config.getStartUrl());
+        super.loadUrl("file:///android_asset/www/index.html");
+
+    }
+}
