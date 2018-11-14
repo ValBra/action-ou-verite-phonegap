0a925a750f347f29dcd66e18409325fc9a06b397
ValBra <valere.brable8@etu.univ-lorraine.fr>
2018-11-14 08:29:46 -0500

    Creation du modele applicatif

---
+++ app/src/main/assets/www/action/application.js
@@ -0,0 +1,47 @@
+(function(){
+	var instance = this;
+	
+	var lancer = function(){
+		this.actionOuVeriteDAO = new actionOuVeriteDAO();
+		window.addEventListener("hashchange",naviguer);
+		naviguer();
+	}
+	
+	var naviguer = function(){
+		var hash = window.location.hash;
+		if(!hash){
+			this.listeAction = this.actionDAO.lister();
+			this.listeVerite = this.veriteDAO.lister();
+			var listeActionVue = new ListeActionVue(instance.listeAction);
+			var listeVeriteVue = new ListeVeriteVue(instance.listeVerite);
+			listeActionVue.afficher();
+			listeVeriteVue.afficher();
+		}
+	}
+	
+	var actionEnregistrerAction = function(action){
+		this.actionDAO.ajouter(action);
+		naviguerAccueil();
+	}
+	
+	var actionModifierAction = function(action){
+		this.actionDAO.modifier(action);
+		naviguerAccueil();
+	}
+	
+	var actionEnregistrerVerite = function(verite){
+		this.veriteDAO.ajouter(verite);
+		naviguerAccueil();
+	}
+	
+	var actionModifierVerite = function(Verite){
+		this.veriteDAO.modifier(Verite);
+		naviguerAccueil();
+	}
+	
+	var naviguerAccueil = function(){
+		window.location.hash = "";
+	}
+	lancer();
+	
+})();
