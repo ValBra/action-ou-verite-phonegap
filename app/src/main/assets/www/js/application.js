3bafb109de579b160b5b2b5121a3573d6d58be6f
AlexandreCM <alexandre.mezeray@gmail.com>
2018-11-14 11:13:34 -0500

    page lister verite

--- app/src/main/assets/www/js/application.js
+++ app/src/main/assets/www/js/application.js
@@ -15,17 +15,23 @@
 			var listeActionVue = new ListeActionVue(instance.listeAction);
 			listeActionVue.afficher();
 		}
+		else if(hash.match(/^#liste-action/))
+        {
+            this.listeAction = this.actionDAO.lister();
+			var listeActionVue = new ListeActionVue(instance.listeAction);
+			listeActionVue.afficher();
+		}
 		else if(hash.match(/^#ajouter-action/))
         {
             var ajouterActionVue = new AjouterActionVue(actionEnregistrerAction);
             ajouterActionVue.afficher();
         }
-		/*else if(hash.match(/^#liste-action/))
+		else if(hash.match(/^#liste-verite/))
         {
-            this.listeAction = this.actionDAO.lister();
-			var listeActionVue = new ListeActionVue(instance.listeAction);
-			listeActionVue.afficher();
-		}*/
+            this.listeVerite = this.veriteDAO.lister();
+			var listeVeriteVue = new ListeVeriteVue(instance.listeVerite);
+			listeVeriteVue.afficher();
+		}
 	}
 	
 	var actionEnregistrerAction = function(action){
