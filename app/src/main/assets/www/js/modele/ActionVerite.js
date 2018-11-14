2c646c2e0c91113d231c92f02701cb370e2b611c
Chloé VIEILLE <chloevieille@hotmail.com>
2018-11-14 09:11:14 -0500

    Modification modele ActionVerite

--- app/src/main/assets/www/js/modele/ActionVerite.js
+++ app/src/main/assets/www/js/modele/ActionVerite.js
@@ -1,5 +1,9 @@
-var ActionVerite = function(phrase, id)
+var ActionVerite = function(phrase, type, theme, difficulte, createur, id)
 {
     this.id = id;
     this.phrase = phrase;
+    this.type = type;
+    this.theme = theme;
+    this.difficulte = difficulte;
+    this.createur = createur;
 }
