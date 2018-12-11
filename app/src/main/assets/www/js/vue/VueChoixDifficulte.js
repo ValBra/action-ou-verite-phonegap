84a6a38a57e47af6e86d306a52ff4d663a82c087
Valere Brable <valere.brable8@etu.univ-lorraine.fr>
2018-12-11 11:53:12 -0500

    Page choix difficulte

---
+++ app/src/main/assets/www/js/vue/VueChoixDifficulte.js
@@ -0,0 +1,24 @@
+var JeuVue = (function()
+{
+    var pageChoixDifficulte = document.getElementById("page-difficulte").innerHTML;
+
+    return function()
+    {
+        this.afficher = function()
+        {
+            document.getElementsByTagName("body")[0].innerHTML = pageChoixDifficulte;
+
+            var formulaireAjouter = document.getElementById("formulaire-choix-difficulte");
+            formulaireAjouter.addEventListener("submit",enregistrerChoixDifficulte);
+        }
+
+        var enregistrerChoixDifficulte = function(evenement)
+        {
+            
+            evenement.preventDefault();
+
+            var theme = document.getElementById("theme").value;
+            var difficulte = document.getElementById("difficulte").value;
+        }
+    };
+})()
